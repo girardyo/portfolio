98716b18ab689b8da3fe96a3b3281bf1ce00f610
Yohan <yohangirard4pro@gmail.com>
2018-10-16 10:41:48 +0200

    tab color js +booststrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ function setColor(){
     var button2 = document.getElementById('btn2');
     button2.style.visibility = 'visible';
     var titre = document.getElementById('titre');
-    titre.style.color = 'white';
+    titre.style.color = 'black';
 }
 function setColor2(){
     document.body.style.backgroundColor = 'rgb(231, 231, 231)';
@@ -14,5 +14,34 @@ function setColor2(){
     var button2 = document.getElementById('btn');
     button2.style.visibility = 'visible';
     var titre = document.getElementById('titre');
-    titre.style.color = 'black';
+    titre.style.color = 'white';
 }
+
+var i =0;
+var t = null;
+
+function changecolor(){
+    var couleurs = ['blue', 'red', 'yellow'];
+    console.log (couleurs.length);
+    if (i< couleurs.length){
+        document.body.style.backgroundColor = couleurs[i];
+        i++;
+        console.log(couleurs[i]);
+    } 
+    else {
+        stopInterval();
+    }
+        
+    
+    
+}
+function timeout (){
+    t =setInterval(changecolor, 1000); 
+}
+
+function stopInterval(){
+    clearInterval(t);
+}
+
+
+
