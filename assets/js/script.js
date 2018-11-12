f3519a594806f2b34b458c7c4801ca3a03460d87
Yohan <yohangirard4pro@gmail.com>
2018-11-12 09:54:26 +0100

    function night and day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function setColor(){
+/*function setColor(){
     document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.445)';
     var button = document.getElementById('btn');
     button.style.visibility = 'hidden';
@@ -16,12 +16,50 @@ function setColor2(){
     var titre = document.getElementById('titre');
     titre.style.color = 'white';
 }
-
+*/
 var i =0;
 var t = null;
 
-function changecolor(){
-    var couleurs = ['blue', 'red', 'yellow'];
+function changecolornuit(){
+    
+    var button = document.getElementById('btn');
+    button.style.visibility = 'hidden';
+    var button2 = document.getElementById('btn2');
+    button2.style.visibility = 'visible';
+    var titre = document.getElementById('titre');
+    titre.style.color = 'white';
+    var h2 = document.getElementById('h2');
+    h2.innerHTML = "Bonsoir je suis Yohan Girard";
+
+    var couleurs = ['#f9f9f9', '#d6d4d4', '#969696'];
+    console.log (i);
+    if (i< couleurs.length){
+        document.body.style.backgroundColor = couleurs[i];
+        i++;
+        console.log(couleurs[i]);
+    } 
+    else {
+        i = 0;
+        clearInterval(t);
+
+    }       
+}
+
+function nuitprogressif (){
+    t =setInterval(changecolornuit, 1000); 
+}
+
+function changecolorjour(){
+    var button = document.getElementById('btn2');
+    button.style.visibility = 'hidden';
+    var button2 = document.getElementById('btn');
+    button2.style.visibility = 'visible';
+    var titre = document.getElementById('titre');
+    titre.style.color = 'black';
+    var h2 = document.getElementById('h2');
+    h2.innerHTML = "Bonjour je suis Yohan Girard";
+
+    var couleurs = ['#969696', '#d6d4d4', '#f9f9f9'];
     console.log (couleurs.length);
     if (i< couleurs.length){
         document.body.style.backgroundColor = couleurs[i];
@@ -29,19 +67,41 @@ function changecolor(){
         console.log(couleurs[i]);
     } 
     else {
-        stopInterval();
+        i = 0;
+        clearInterval(t);
     }
-        
-    
-    
 }
-function timeout (){
-    t =setInterval(changecolor, 1000); 
-}
-
-function stopInterval(){
-    clearInterval(t);
+function jourprogressif (){
+    t =setInterval(changecolorjour, 1000); 
 }
 
 
+
+/*
+$(document).ready(function() {
+    $('#button').click(function(){
+        $(this).myFunction2();
+     });
+
+     $.fn.myFunction = function() { 
+        var couleurs = ['blue', 'red', 'yellow'];
+        console.log (couleurs.length);
+        if (i< couleurs.length){
+            document.body.style.backgroundColor = couleurs[i];
+            i++;
+            console.log(couleurs[i]);
+        } 
+        else {
+            stopInterval();
+        }
+     }
+     $.fn.myFunction2 = function() {
+        t =setInterval(changecolor, 1000); 
+     }
+
+     $.fn.myFunction3 = function() {
+        clearInterval(t);
+    }
+
+});*/
 
